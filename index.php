40e0e3b2b9552060058b0f033ba74998721a61de
Veta2000 <elizter121@gmail.com>
2024-10-20 22:34:40 +0300

    paymentMeth

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'classes/Vehicle.php';
+require_once 'classes/Car.php';
+require_once 'classes/Motorcycle.php';
+require_once 'classes/PaymentMethod.php';
+require_once 'classes/CreditCardPayment.php';
+require_once 'classes/RentalSystem.php';
